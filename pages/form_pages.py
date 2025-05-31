d04c007c1a2c572d5ec8ee875a886022c2cd9a13
Mor138 <morovichmor@gmail.com>
2025-05-31 16:01:43 +0300

    Delete form_pages.py

--- pages/form_pages.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout
-from form_renderer import render_form
-
-class DynamicFormPage(QWidget):
-    def __init__(self, tab_key):
-        super().__init__()
-        layout = QVBoxLayout(self)
-        layout.setContentsMargins(40, 30, 40, 30)
-        layout.addWidget(render_form(tab_key))
